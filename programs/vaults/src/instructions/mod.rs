ba7e7a103bd68e6b0f469e84f07c61887b705b7e
Chase Whipple <chasewhip20@gmail.com>
2023-02-04 01:21:51 -0700

    inital

---
+++ programs/vaults/src/instructions/mod.rs
@@ -0,0 +1,9 @@
+pub use deposit::*;
+pub use withdraw::*;
+pub use init_vault::*;
+pub use init_group::*;
+
+pub mod deposit;
+pub mod withdraw;
+pub mod init_vault;
+pub mod init_group;
--- programs/vaults/src/lib.rs
+++ programs/vaults/src/lib.rs
@@ -1,7 +1,11 @@
+mod instructions;
+mod state;
+
 use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// Controller
 #[program]
 pub mod vaults {
     use super::*;
