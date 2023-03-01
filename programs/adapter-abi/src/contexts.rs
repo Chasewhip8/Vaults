ba40bd2e1c14fe728c08e60b7783c170025450e7
Chase Whipple <chasewhip20@gmail.com>
2023-02-28 21:25:32 -0700

    Fill out some abi

--- programs/adapter-abi/src/contexts.rs
+++ programs/adapter-abi/src/contexts.rs
@@ -11,20 +11,22 @@ pub struct IDeposit<'info> {
 
 #[derive(Accounts)]
 pub struct IRedeem<'info> {
+    #[account(
+        owner = VAULTS_PROGRAM.key()
+    )]
+    pub _ensure_vaults_signed: Signer<'info>,
     pub authority: Signer<'info>,
 }
 
 #[derive(Accounts)]
 pub struct ICrank<'info> {
-    pub authority: Signer<'info>,
+
 }
 
 #[derive(Accounts)]
-pub struct IAdminWithdraw<'info> {
-    pub vault_authority: Signer<'info>,
-}
-
-#[derive(Accounts)]
-pub struct IAdminDeposit<'info> {
-    pub vault_authority: Signer<'info>,
+pub struct Execute<'info> {
+    #[account(
+        owner = VAULTS_PROGRAM.key()
+    )]
+    pub _ensure_vaults_signed: Signer<'info>,
 }
--- programs/adapter-abi/src/lib.rs
+++ programs/adapter-abi/src/lib.rs
@@ -12,7 +12,38 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod adapter_abi {
     use super::*;
 
+    /// Instruction to handle deposits from the vaults program, the vaults program has no idea what this program does
+    /// except for it's return value.
+    ///
+    /// # Arguments
+    ///
+    /// * `ctx`: Instruction Context
+    /// * `amount`: The adapter adjusted deposit amount, aka total_deposit * ratio
+    ///
+    /// returns: Result<u64, Error>, where the u64 is the providers updated balance used to calculate minted receipt assets
     pub fn deposit(ctx: Context<IDeposit>, amount: u64) -> Result<u64> {
-        Ok(0)
+        msg!("Entered adapter_abi::deposit({}) instruction, returning fake value of 1", amount);
+        Ok(1)
+    }
+
+    /// Instruction to handle redemptions from the vaults program, the vaults program has no idea what this program does
+    ///
+    /// # Arguments
+    ///
+    /// * `ctx`: Instruction Context
+    /// * `amount`: The adapter adjusted redeem amount
+    ///
+    /// returns: Result<(), Error>, where the u64 is the providers updated balance used to calculate minted receipt assets
+    pub fn redeem(ctx: Context<IRedeem>, amount: u64) -> Result<()> {
+        msg!("Entered adapter_abi::redeem({}) dummy instruction", amount);
+        Ok(())
+    }
+
+    /// Instruction to crank and return the providers balance.
+    ///
+    /// returns: Result<u64, Error>, where the u64 is the providers updated balance used to calculate minted receipt assets
+    pub fn crank(ctx: Context<ICrank>) -> Result<u64> {
+        msg!("Entered adapter_abi::crank dummy instruction");
+        Ok(1)
     }
 }
