249bb94648e1db4f7bea4ec56f61d38ae0dd3650
Max Monciardini <maxmonciardini@icloud.com>
2023-02-28 19:16:26 -0700

    web (feat): add vault expansion button (not yet functional)

--- packages/web/components/Dashboard/YourVaultsCard.tsx
+++ packages/web/components/Dashboard/YourVaultsCard.tsx
@@ -4,41 +4,44 @@ import Table from "@components/Table/Table";
 import TableCell from "@components/Table/TableCell";
 import TableHeader from "@components/Table/TableHeader";
 import TableRow from "@components/Table/TableRow";
-import { ArrowsPointingOutIcon } from "@heroicons/react/20/solid";
+import {
+  ArrowsPointingOutIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/20/solid";
 import React from "react";
 
 const tableRows = [
   {
     vault: "J-Sol-Eth",
-    expiration: "33 days",
+    expiration: "(expand)",
     amount: "153.32",
     amountDollar: "$1,532.32",
     percentChange: "5.32%",
   },
   {
     vault: "J-SFXS-USDC",
-    expiration: "25 days",
+    expiration: "(expand)",
     amount: "931.25",
     amountDollar: "$10,123.56",
     percentChange: "3.72%",
   },
   {
     vault: "J-BTC-USDT",
-    expiration: "12 days",
+    expiration: "(expand)",
     amount: "140.02",
     amountDollar: "$2,312.98",
     percentChange: "2.15%",
   },
   {
     vault: "J-DAI-USDT",
-    expiration: "18 days",
+    expiration: "(expand)",
     amount: "876.81",
     amountDollar: "$11,975.27",
     percentChange: "4.28%",
   },
   {
     vault: "J-SOL-USDC",
-    expiration: "45 days",
+    expiration: "(expand)",
     amount: "327.98",
     amountDollar: "$6,429.81",
     percentChange: "8.91%",
@@ -50,7 +53,7 @@ type Props = {};
 const YourVaultsCard = (props: Props) => {
   return (
     <Card className="flex flex-1 flex-col">
-      <div className="flex flex-1 flex-row justify-between items-center">
+      <div className="flex flex-1 flex-row justify-between items-start">
         <div className="flex flex-col">
           <h2 className="text-2xl font-bold">Your Vaults</h2>
           <p className="text-gray-400">Track your investments</p>
@@ -64,6 +67,7 @@ const YourVaultsCard = (props: Props) => {
       <Table
         className="overflow-y-auto overflow-x-hidden"
         headers={[
+          <TableHeader key="Expand" />,
           <TableHeader key="Vault">Vault</TableHeader>,
           <TableHeader key="Expiration">Expiration</TableHeader>,
           <TableHeader key="Amount">Amount</TableHeader>,
@@ -72,8 +76,13 @@ const YourVaultsCard = (props: Props) => {
         ]}
         rows={tableRows.map((row) => (
           <TableRow key="1">
-            <TableCell>{row.vault}</TableCell>
-            <TableCell>{row.expiration}</TableCell>
+            <TableCell>
+              <button className="cursor-pointer hover:scale-125 transition-all duration-200">
+                <ChevronRightIcon className="h-5 w-5" />
+              </button>
+            </TableCell>
+            <TableCell className="font-bold">{row.vault}</TableCell>
+            <TableCell className="italic">{row.expiration}</TableCell>
             <TableCell>{row.amount}</TableCell>
             <TableCell>{row.amountDollar}</TableCell>
             <TableCell>{row.percentChange}</TableCell>
--- packages/web/components/Table/TableHeader.tsx
+++ packages/web/components/Table/TableHeader.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import React from "react";
 
 type Props = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
 };
 
